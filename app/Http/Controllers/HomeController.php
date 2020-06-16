b8d897511c89476f410d2ced14665da89c3e93e3
Nmaidana <nelson_lmaidana@hotmail.com>
2020-06-15 23:02:56 -0400

    Delete Login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+       // $this->middleware('auth');
     }
 
     /**
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('welcome');
     }
     public function create(Request $request)
     {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,104 +1,37 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" style="margin-left: 0px;">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>AdminLTE 3 | Starter</title>
+    <nav class="main-header navbar navbar-expand navbar-dark navbar-success" style="margin-left: 0px;">
+
+        <div class="container">
+
+            <a class="navbar-brand">
+                <b>Formulario </b>Clase Media
+            </a>
+        </div>
+        </div>
+
+    </nav>
     <link rel="stylesheet" href="{{ asset('wolfsyncro/adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('wolfsyncro/adminlte3/dist/css/adminlte.min.css') }}">
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     @yield('css')
 </head>
 
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini" style="margin-left: 0px;">
     <div class="wrapper">
-    <nav class="main-header navbar navbar-expand navbar-dark navbar-lightblue">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="{{url('home')}}" class="nav-link">Inicio</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                            Cerrar Sesion
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="{{url('home')}}" class="brand-link">
-                <img src="{{url('wolfsyncro/adminlte3/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
-            <div class="sidebar">
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{url('wolfsyncro/adminlte3/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">{{Auth::user()->name}}</a>
-                        <p style="color:grey;font-size:12px">{{Auth::user()->email}}</p>
+        <div class="content-wrapper" style="margin-left: 0px;"><br></br>
 
-                    </div>
-                </div>
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
-                        <li class="nav-item">
-                            <a href="{{url('home')}}" class="nav-link {{ request()->is('home') ? 'active' : '' }}">
-                                <i class="nav-icon fa fa-home"></i>
-                                <p>
-                                    Home
-                                    <span class="right badge badge-success">Nuevo</span>
-                                </p>
-                            </a>
-                        </li>
-                   
-                    </ul>
-                </nav>
-            </div>
-        </aside>
-        <div class="content-wrapper">
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="content">
+            <div class="content" style=" position: absolute; right: 350px;  width: 800px;">
                 <div class="container-fluid">
                     @yield('content')
                 </div>
             </div>
         </div>
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-inline">
-                Anything you want
-            </div>
-            <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
-        </footer>
     </div>
     <script src="{{ asset('wolfsyncro/adminlte3/plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('wolfsyncro/adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>   
+    <script src="{{ asset('wolfsyncro/adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('wolfsyncro/adminlte3/dist/js/adminlte.min.js') }}"></script>
 
     @yield('scripts')
